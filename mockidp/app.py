079cc6ecb5821af4e6f2827e1770de0b7371ecfa
A. Diamond <andrew.diamond@simplethread.com>
2024-06-19 13:21:10 -0400

    Added a secret key to prevent flash errors

--- mockidp/app.py
+++ mockidp/app.py
@@ -4,6 +4,7 @@ Moving this out of __init__ in order to make e.g. setup.py not require dependenc
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key='this is not secure but it prevents errors'
 
 import mockidp.saml.routes
 import mockidp.routes
